03de12a6663719206d9385f7720efb4780236c63
Ammar Alakkad <am.alakkad@gmail.com>
2015-11-09 14:52:50 +0200

    Extract header, footer and sidebar to external files

---
+++ sidebar.php
@@ -0,0 +1,20 @@
+<h1>Sidebar</h1>
+<div class="block">
+    <img src="http://dummyimage.com/400x400/333/eee">
+</div>
+<div class="block">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="block">
+    <a href="#">Link</a>
+    <p>ASDASD ASD ASD AS DASD AS D</p>
+    <div>
+        <img src="http://dummyimage.com/400x400/333/eee">
+    </div>
+    <p>ASDASD ASD ASD AS DASD AS D</p>
+</div>
