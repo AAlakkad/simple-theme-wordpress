cac6377e97e41cb3ff91fdf807e8642307e70483
Ammar Alakkad <am.alakkad@gmail.com>
2015-11-10 11:16:25 +0200

    Extract style files into wp_enqueue_scripts action

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', function() {
+    wp_register_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css', [], '3.3.5', 'all' );
+    wp_register_style('our-custom-css', get_template_directory_uri() . '/custom.css', ['bootstrap']);
+
+    wp_enqueue_style('our-custom-css');
+} );
