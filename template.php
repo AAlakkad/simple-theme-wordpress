98cc475a82a60bfeb5b56ebada26fb3b1e8da680
Sara Aushar <sa.au91@gmail.com>
2015-11-11 12:50:34 +0200

    new_template

---
+++ template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: test_template;
+ * 
+ */
+get_header(); 
+?>
+ <div class="container">
+        <div class="row">
+             <h1>welcome</h1>            
+        </div>
+    </div
+   <?php get_footer();?>
